48aabbc7854524b2bc30c197b7100a9108af5e64
Razor7w <seba.carroza@gmail.com>
2020-11-29 18:10:30 -0300

    [Feat] - Se modifica la vista para agregar noticias - Se agrega ruta para cargar categorias de las noticias

--- src/app/periodistas/screens/periodista-home-screen/periodista-home-screen.component.ts
+++ src/app/periodistas/screens/periodista-home-screen/periodista-home-screen.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { resolvePtr } from 'dns';
+import { backend } from '../../../axios';
 
+interface Categorias {
+  id_categoria: number;
+  nombre_categoria: string;
+}
 @Component({
   selector: 'app-periodista-home-screen',
   templateUrl: './periodista-home-screen.component.html',
   styleUrls: ['./periodista-home-screen.component.less']
 })
 export class PeriodistaHomeScreenComponent implements OnInit {
-
-  constructor() { }
+  formularioAgregarNoticia: FormGroup;
+  constructor(private fb: FormBuilder) {}
+  categorias: Categorias[] =  [{
+    id_categoria: 0,
+    nombre_categoria: ''
+  }];
+  seleccionado: Categorias = {
+    id_categoria: 0,
+    nombre_categoria: ''
+  };
 
   ngOnInit(): void {
+    this.formularioAgregarNoticia = this.fb.group({
+      titulo: ['', Validators.required],
+      cuerpo: ['', Validators.required],
+      categoria: ['', Validators.required]
+    });
+
+    backend.get('categorias')
+      .then(resp => {
+        // console.log(resp);
+        this.categorias = resp.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  agregarNoticia(): void{
+    const noticia = this.formularioAgregarNoticia.value;
+    console.log(noticia);
+    backend.post('noticias', noticia)
+      .then(resp => {
+        // TODO: Acción post agregar noticia
+        console.log(resp);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
 }
